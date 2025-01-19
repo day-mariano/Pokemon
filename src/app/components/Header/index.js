9cdef491dcf746a6131a6c6fcbe264a70dc44e52
Dayana Mariano <dayanaraquelle@gmail.com>
2025-01-19 20:07:41 -0300

    :sparkles: feat: Add Header and Footer

---
+++ src/app/components/Header/index.js
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../../page.module.css";
+
+export default function Header() {
+  return (
+    <>
+      <header className={styles.header}>
+        <Link
+          href="/" 
+          target="_self"
+        //   className={styles.a}
+        >
+            <Image
+            // aria-hidden
+            src="/pokemonlogo.png"
+            alt="File icon"
+            width={158.32}
+            height={64.04}
+            />
+        </Link>
+
+
+        <nav className={styles.nav}>
+          <Link
+            href="https://pokemontcg.io/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Documentação
+          </Link>
+        </nav>
+      </header>
+    </>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,38 +1,35 @@
-import Image from "next/image";
+import Link from "next/link";
 import styles from "./page.module.css";
+import Header from "./components/Header";
 
 export default function Home() {
   return (
     <div className={styles.page}>
+      <Header/>
       <main className={styles.main}>
     
-        <div className={styles.ctas}>
-          <a
+        <div className={styles}>
+          <Link
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
             className={styles.secondary}
           >
-            Read our docs
-          </a>
-          <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
+            lista
+          </Link>
+          
         </div>
       </main>
       <footer className={styles.footer}>
-       Desenvolvido por Dayana Mariano 2025
+       Desenvolvido por 
+        <Link
+          href="https://dayanamariano.dev/"
+          target="_blank"
+          rel="noopener noreferrer"
+          >
+          Dayana Mariano
+        </Link>
+        2025
       </footer>
     </div>
   );
