a54f8148735bc740f2bc705a6bc482b048c0b6a5
Dayana Mariano <dayanaraquelle@gmail.com>
2025-01-21 12:36:34 -0300

    :sparkles: feat: Add Card Component"

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'images.pokemontcg.io',
+            port: '',
+            pathname: '/**',
+            search: '',
+          },
+        ],
+      },
+
+};
 
 export default nextConfig;
---
+++ src/app/components/Card/index.js
@@ -0,0 +1,54 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../../page.module.css";
+
+
+
+export default function Card(props) {
+  const card = props.card;
+
+  return (
+    <li>
+      {/* <Image
+        src={
+          "https://prices.pokemontcg.io/cardmarket/" + card.id
+        }
+        alt={card.name}
+        width={280}
+        height={208}
+      /> */}
+      <Image
+        src={card.images.small}
+        alt={card.name}
+        width={280}
+        height={394.28}
+      />
+      <div className={styles.cardInfo}>
+        <div className={styles.info}>
+            <h2 > {card.name} </h2>
+            <p>Type {card.types}</p>
+        </div>
+        <div className={styles.info}>
+            <p>{card.rarity}</p>
+            <Image
+                src={card.set.images.symbol}
+                alt={card.name}
+                width={28}
+                height={28}
+                className={styles.infoImage}
+                  />
+        </div>
+        {/* <Link
+          to={{
+            pathname: "/movie/" + card.id,
+          }}
+          state={card}
+          id="buttonDetails"
+        >
+          {" "}
+          Details
+        </Link> */}
+      </div>
+    </li>
+  );
+}
